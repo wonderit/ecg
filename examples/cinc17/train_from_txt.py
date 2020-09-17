e9edcaf6e3e5dbcf0bf6059a5930cfd63101b1f1
wonderit <wonsuk@broadinstitute.org>
2020-09-17 15:04:26 -0400

    shell script for training cinc17

--- examples/cinc17/train_from_txt.py
+++ examples/cinc17/train_from_txt.py
@@ -87,7 +87,7 @@ class NetMaxpool(nn.Module):
                                padding=(self.kernel_size // 2))
         self.conv5 = nn.Conv1d(self.channel_size, self.channel_size, kernel_size=self.kernel_size,
                                padding=(self.kernel_size // 2))
-        self.fc1 = nn.Linear(1024, 64)
+        self.fc1 = nn.Linear(512, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 4)
         self.dropout1 = nn.Dropout(0.2)
