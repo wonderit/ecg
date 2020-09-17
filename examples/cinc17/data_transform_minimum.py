ed46f6fa1e5e82532566f77e0828761b9c2c774d
wonderit <wonsuk@broadinstitute.org>
2020-09-17 13:36:08 -0400

    shell script for training cinc17

--- examples/cinc17/data_transform_minimum.py
+++ examples/cinc17/data_transform_minimum.py
@@ -19,6 +19,11 @@ def apply_threshold(arr, thres):
     arr[arr < -thres] = thres
     return arr
 
+def compute_mean_std(x):
+    x = np.hstack(x)
+    return (np.mean(x).astype(np.float32),
+           np.std(x).astype(np.float32))
+
 # X_train = X_train.reshape([7676, 1, 256, 8]).mean(3).mean(1)
 # X_test = X_test.reshape([852, 1, 256, 8]).mean(3).mean(1)
 X_train = X_train.reshape([7676, 1, 512, 2560 // 512]).mean(3).mean(1)
@@ -37,10 +42,17 @@ filtered_test_x, _, _ = st.filter_signal(signal=X_test,
                                           order=order,
                                           frequency=[3, 45],
                                           sampling_rate=100)
-X_threshold = apply_threshold(filtered_train_x, 4000)
-test_x_thres = apply_threshold(filtered_test_x, 4000)
-current_x = scale_maxabs(X_threshold, np.max(np.abs(X_threshold)), 30)
-current_test_x = scale_maxabs(test_x_thres, np.max(np.abs(test_x_thres)), 30)
+
+mean, std = compute_mean_std(filtered_train_x)
+print(mean, std)
+
+# X_threshold = apply_threshold(filtered_train_x, 4000)
+# test_x_thres = apply_threshold(filtered_test_x, 4000)
+# current_x = scale_maxabs(X_threshold, np.max(np.abs(X_threshold)), 30)
+# current_test_x = scale_maxabs(test_x_thres, np.max(np.abs(test_x_thres)), 30)
+
+current_x = (filtered_train_x - mean) / std
+current_test_x = (filtered_test_x - mean) / std
 import os
 data_dir = '../../minimum_data'
 
