cbb100d05b09129c863d1cc848dd3444362121cc
wonderit <wonsuk@broadinstitute.org>
2020-09-23 11:28:08 -0400

    shell script for training cinc17

--- examples/cinc17/notebooks/eval.py
+++ examples/cinc17/notebooks/eval.py
@@ -80,6 +80,8 @@ model_path = "../../../saved/cinc17/1600873097-592/0.883-0.654-024-0.775-0.685.h
 model_path = "../../../saved/cinc17/1600873600-115/0.737-0.699-030-0.688-0.727.hdf5"
 # regular w/o bn, w/ dropout 0.2, ws : 40
 model_path = "../../../saved/cinc17/1600873962-480/0.436-0.848-028-0.407-0.855.hdf5"
+# regular w/o bn, w/ dropout 0.2, ws : 50
+model_path = "../../../saved/cinc17/1600874444-895/0.403-0.854-020-0.369-0.866.hdf5"
 
 data = load.load_dataset(data_path)
 preproc = util.load(os.path.dirname(model_path))
